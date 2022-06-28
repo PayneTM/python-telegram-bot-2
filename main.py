fa2a2879a592821fb0aeb4d7b611a6d78b5ddc37
paynetm <maksim.tantsyura@gmail.com>
2022-06-28 14:24:06 +0300

    Added config file

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from TeamSubscription import TeamSubscription
 from TeamSubscriptionRepository import TeamSubscriptionRepository
 from TeamSubscriptionRepositoryCache import TeamSubscriptionRepositoryCache
 from telegram import Update
-from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes, ApplicationBuilder, ContextTypes, CommandHandler, InlineQueryHandler
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes, ApplicationBuilder, ContextTypes, CommandHandler
+import configparser
 
 def main():
-    application = ApplicationBuilder().token('TOKEN').build()
+    application = ApplicationBuilder().token(config['AppConfig']['token']).build()
 
     main_repo.setup()
     cache_repo.setup()
@@ -23,8 +24,11 @@ def main():
     
     application.run_polling()
 
-main_repo = TeamSubscriptionRepository("DBName")
-cache_repo = TeamSubscriptionRepositoryCache("DBName")
+config = configparser.ConfigParser()
+config.read('main.ini')
+
+main_repo = TeamSubscriptionRepository(config['AppConfig']['db_name'])
+cache_repo = TeamSubscriptionRepositoryCache(config['AppConfig']['db_name'])
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -34,14 +38,14 @@ logging.basicConfig(
 async def register_team(update: Update, context: ContextTypes.DEFAULT_TYPE):
     result = cache_repo.add_item(TeamSubscription(update.effective_chat.id, update.message.text))
     if not result:
-        await context.bot.send_message(chat_id=update.effective_chat.id, text="Error occured")
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Error occured.")
 
 async def save(update: Update, context: ContextTypes.DEFAULT_TYPE):
     result = cache_repo.get_items(update.effective_chat.id)
     saved = main_repo.add_item(result)
     message = ""
     if not saved:
-        message = "Error occured"
+        message = "Nothing saved."
     else:
         message = "Success!"
         cache_repo.delete_item(update.effective_chat.id)
